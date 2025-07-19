00ae58e639f9d71fdec39d4362904b6ed05152ec
man7788 <jerman1841@gmail.com>
2025-07-20 05:54:45 +0800

    feat: add messageRouter to handle message-related routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('node:path');
 
 const indexRouter = require('./routes/indexRouter');
+const messageRouter = require('./routes/messageRouter');
 
 const assetsPath = path.join(__dirname, 'public');
 
@@ -12,6 +13,7 @@ app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
+app.use('/messages', messageRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
