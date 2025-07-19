aa7ec1cc989c9cfbb51957f6a1556d4293579237
man7788 <jerman1841@gmail.com>
2025-07-20 02:12:59 +0800

    feat: enable URL-encoded data parsing for form submissions

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const assetsPath = path.join(__dirname, 'public');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
 
