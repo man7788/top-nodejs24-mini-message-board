dcf1851a584c1a4b8c73cad5ef1addd692d25003
man7788 <jerman1841@gmail.com>
2025-07-19 12:54:26 +0800

    feat: add predefined messages array to index router

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,24 @@ const { Router } = require('express');
 
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+  {
+    text: "What's up?",
+    user: 'David',
+    added: new Date(),
+  },
+];
+
 indexRouter.get('/', (req, res) => {
   res.send('Home');
 });
