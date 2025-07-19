ad4b8e9b688dd7cc288defafa1016cece2cdf868
man7788 <jerman1841@gmail.com>
2025-07-20 06:57:41 +0800

    refactor: simplify indexRouter by delegating message handling to controller functions

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,26 +1,16 @@
 const { Router } = require('express');
-const db = require('../db.js');
+const {
+  getAllMessages,
+  getMessageForm,
+  createMessage,
+} = require('../controllers/indexController');
 
 const indexRouter = Router();
 
-indexRouter.get('/', async (req, res) => {
-  const messages = await db.getAllMessages();
-  res.render('index', { title: 'Mini Messageboard', messages: messages });
-});
+indexRouter.get('/', getAllMessages);
 
-indexRouter.get('/new', (req, res) => {
-  res.render('form', { title: 'New Message' });
-});
+indexRouter.get('/new', getMessageForm);
 
-indexRouter.post('/new', async (req, res) => {
-  const count = await db.getMessageCount();
-  await db.createMessage({
-    id: count + 1,
-    text: req.body.text,
-    user: req.body.name,
-    added: new Date(),
-  });
-  res.redirect('/');
-});
+indexRouter.post('/new', createMessage);
 
 module.exports = indexRouter;
