c201621c244d75b3b9b3c3d20d5188baba273682
man7788 <jerman1841@gmail.com>
2025-07-20 07:04:19 +0800

    refactor: replace inline message retrieval with getMessage controller function

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,18 +1,9 @@
 const { Router } = require('express');
 const db = require('../db.js');
+const { getMessage } = require('../controllers/messageController');
 
 const messageRouter = Router();
 
-messageRouter.get('/message/:messageId', async (req, res) => {
-  const { messageId } = req.params;
-  const message = await db.getMessageById(Number(messageId));
-  res.render('messages/message', {
-    message: message.text,
-    user: message.user,
-    added: message.added,
-    id: message.id,
-    newPage: true,
-  });
-});
+messageRouter.get('/message/:messageId', getMessage);
 
 module.exports = messageRouter;
