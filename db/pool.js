73b595d1d56fa1e6b09100dfc78ce660966de7ee
man7788 <jerman1841@gmail.com>
2025-07-23 09:51:34 +0800

    feat: install dontenv

---
+++ db/pool.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+// All of the following properties should be read from environment variables
+// We're hardcoding them here for simplicity
+module.exports = new Pool({
+  host: process.env.PGHOST, // or wherever the db is hosted
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT, // The default port
+});
