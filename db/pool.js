d782a3154daa6a46107001a639fa30317d439565
man7788 <jerman1841@gmail.com>
2025-07-24 01:23:06 +0800

    fix: remove unnessary comment

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 // All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
 module.exports = new Pool({
   host: process.env.PGHOST, // or wherever the db is hosted
   user: process.env.PGUSER,
