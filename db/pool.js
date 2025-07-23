791291966075c83ce8762451366179190b5e9c6c
man7788 <jerman1841@gmail.com>
2025-07-24 03:45:24 +0800

    feat: update pool to use connection string

--- db/pool.js
+++ db/pool.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 // All of the following properties should be read from environment variables
+// module.exports = new Pool({
+//   host: process.env.PGHOST, // or wherever the db is hosted
+//   user: process.env.PGUSER,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT, // The default port
+// });
+
 module.exports = new Pool({
-  host: process.env.PGHOST, // or wherever the db is hosted
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT, // The default port
+  connectionString: process.env.PGCONNECTSTRING,
 });
